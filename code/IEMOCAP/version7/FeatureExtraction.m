afd162202413e515674dc2b03726352d33efb821
YuanGongND <ygong1@nd.edu>
2016-12-30 17:49:44 -0500

    Reorgnize Folders

---
+++ code/IEMOCAP/version7/FeatureExtraction.m
@@ -0,0 +1,11 @@
+function [ output_args ] = FeatureExtraction( setting )
+% use openSmile tools to extract emotion related features
+
+dirRecording = dir( setting.recordingPath );
+%% the first two is not file 
+for fileIndex = 3 : length( dirRecording )
+    
+end
+
+end
+
