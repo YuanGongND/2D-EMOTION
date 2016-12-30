17cfa86f10f9d9e636cbd98fd90def4ac2f70a14
yuangongnd <ygong1@nd.edu>
2016-12-29 23:24:52 -0500

    Add segmentation code

--- code/IEMOCAP/version7/trySystem.m
+++ code/IEMOCAP/version7/trySystem.m
@@ -1,7 +1,7 @@
 [ recording, Fs ] = audioread ( 'Ses01F_impro01.wav' );
 
-setting.maxsilence = 0;
-setting.minlength = 0;
+setting.maxsilence = 10;
+setting.minlength = 20;
 setting.plothight = 1.5;
 setting.frameLen = 1280;
 setting.segmentLength = 0.5*16000; 
